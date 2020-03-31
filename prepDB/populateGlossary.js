c2aab30ff66aa13f0a6603d31423976fb7849e42
Andrew Briceno <andrewbriceno2000@gmail.com>
2020-03-31 13:09:22 -0400

    Finished prepDB and updated recipe model

--- prepDB/populateGlossary.js
+++ prepDB/populateGlossary.js
@@ -1,1 +1,51 @@
+import * as fs from 'fs'; 
+import mongoose from 'mongoose';
+import GlossaryModel from '../models/GlossaryModel.js';
+import config from '../config/config.js';
+import fastcsv from 'fast-csv';
+
+mongoose.connect(config.db.uri, {useNewUrlParser: true, useUnifiedTopology: true});
+
+var stream=fs.createReadStream('Consider_Herbs_Glossary.csv');
+let glossaryData=[];
+let index=0;
+let csvStream=fastcsv
+    .parse()
+    .on('data',function(data){
+        var glossaryEntry=new GlossaryModel({
+            title: data[0],
+            definition: data[1],
+            usage: data[2],
+            is_published: true,
+        });
+        if (glossaryEntry.title==''){
+            glossaryEntry.title='MISSING TITLE';
+            glossaryEntry.is_published=false;
+        }
+        if (glossaryEntry.definition==''){
+            glossaryEntry.definition='MISSING DEFINITION';
+            glossaryEntry.is_published=false;
+        }
+        if (glossaryEntry.usage==''){
+            glossaryEntry.usage='MISSING USAGE';
+            glossaryEntry.is_published=false;
+        }
+        if (index==0){
+            glossaryData.push(glossaryEntry)
+            index++;
+        }
+        else if (glossaryEntry.title!=glossaryData[index-1].title){
+            glossaryData.push(glossaryEntry);
+            index++;
+        }
+    })
+    .on('end',function(){
+        glossaryData.shift();
+        //console.log(glossaryData);
+        GlossaryModel.insertMany(glossaryData, (err) =>{
+            if (err) throw err;
+        })
+    });
+
+stream.pipe(csvStream);
 console.log("populated Glossary");
