9e285fb8fd41f761a79314b576b1149fd49f6b23
Jerdan1980 <jeremytinana@gmail.com>
2020-04-15 15:38:26 -0400

    Cut extras pt 2

--- routes/forumRouter.js
+++ routes/forumRouter.js
@@ -8,11 +8,6 @@ const forumRouter = express.Router();
 //there will need to be validation later,
 //the user's data will be in 'req.decoded'
 
-//folder functionality?
-forumRouter.get('/get_folders', forum.getFolders);
-	//returns the folders as a list
-	//need to find a way to store all folders, that way you can add a thread to a single folder
-
 //getters
 forumRouter.get('/get_thread', forum.getThreadList); //grabs all the threads
 forumRouter.get('/get_thread/:id', forum.getThread); //grabs one thread (which contains the comments)
