05cb6c8a1b0fb2374a41de3430fdd53c97da1b6e
Jerdan1980 <jeremytinana@gmail.com>
2020-04-17 12:53:15 -0400

    Finish PrepDB

--- prepDB/emptyRemedies.js
+++ prepDB/emptyRemedies.js
@@ -1,14 +1,12 @@
 import mongoose from 'mongoose';
-import RecipeModel from '../models/RecipeModel.js';
 import RemedyModel from '../models/RemedyModel.js';
 import config from '../config/config.js';
 
 mongoose.connect(config.db.uri, {useNewUrlParser: true, useUnifiedTopology: true});
-RecipeModel.deleteMany({}, (err) => {
-    if (err) throw err;
-});
 RemedyModel.deleteMany({}, (err) => {
 	if (err) throw err;
+	else {
+		mongoose.connection.close();
+		console.log("emptied Recipes");
+	}
 });
-mongoose.connection.close();
-console.log("emptied Recipes");
