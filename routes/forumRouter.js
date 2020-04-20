c7d7b0ef047be18f920ba5a14e6d6afef4c96454
Jerdan1980 <jeremytinana@gmail.com>
2020-04-19 23:03:54 -0400

    change comments

--- routes/forumRouter.js
+++ routes/forumRouter.js
@@ -10,9 +10,8 @@ const forumRouter = express.Router();
 
 //getters
 forumRouter.get('/get_post', forum.getPostList); //grabs all the posts
-forumRouter.get('/get_post/:id', forum.getPost); //grabs one thread (which contains the comments)
+forumRouter.get('/get_post/:id', forum.getPost); //grabs one post (which contains the comments)
 forumRouter.get('/get_comment/:post_id/:comment_id', forum.getComment);
-//maybe get individual comment, like reddit does? not too sure how that would work out
 
 //Post functionality
 //Post start the conversation
@@ -20,8 +19,8 @@ forumRouter.post('/add_post', forum.addPost);
 forumRouter.put('/edit_post/:id', forum.editPost);
 forumRouter.delete('/delete_post/:id', forum.deletePost);
 
-//post functionality
-//posts are the comments
+//comment functionality
+//comments reply to the post
 forumRouter.post('/add_comment/:post_id', forum.addComment);
 forumRouter.put('/edit_comment/:post_id/:comment_id', forum.editComment); //not sure if you can stack them this way...
 forumRouter.delete('/delete_comment/:post_id/:comment_id', forum.deleteComment);
