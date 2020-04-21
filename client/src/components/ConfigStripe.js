e8502c88df857288f3e376cddbe1e89073d6c891
Zlata Itkin <zitkin@ufl.edu>
2020-04-20 20:01:34 -0400

    Finally changed stripe transac image, and skeleton of Admin Images panel

--- client/src/components/ConfigStripe.js
+++ client/src/components/ConfigStripe.js
@@ -2,6 +2,7 @@ import React, { Fragment } from "react";
 import StripeCheckout from "react-stripe-checkout";
 import config from "./config";
 import axios from "axios";
+import logosmall from '../resources/logosmall.jpg';
 
 const stripeBtn = (props) => {
 	const publishableKey = config.stripe.test.publishable;
@@ -39,7 +40,7 @@ const stripeBtn = (props) => {
 			amount={props.amount}				// Amount in cents $9.99
 			token={onToken}
 			stripeKey={publishableKey}
-			image="https://considerherbs.com/media/filer_public_thumbnails/filer_public/b8/ca/b8cab25d-08ef-42e2-b9e1-0dbe812a4816/logo.jpg__2500x579_q85_subsampling-2.jpg" //Pop-in header image
+			image={logosmall} //Pop-in header image
 			billingAddress={false}
 		/>
 	);
